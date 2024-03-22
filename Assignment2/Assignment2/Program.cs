db9db369ba245966e0ed6ace3c3f0179f0090549
Jordan Hamilton <jordanh6489@hotmail.com>
2024-03-22 15:04:12 -0400

    Files Created!

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Created By: Jordan Hamilton
+//Date: 16/03/24
+//Purpose: allows the user to manipulate the properties of a rectangle
+class Assingment2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hello world!");
+    }
+}
